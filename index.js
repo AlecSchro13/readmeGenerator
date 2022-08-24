d5bed57497321a2b169567afecea0c622a81669f
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-08-24 13:44:19 -0500

    adding generatemarkdown function to create readme

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const inquirer = require('inquirer');
 const fse = require('fs-extra');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
-//const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-inquirer
-  .prompt([
+const questions = [
     {
         type: 'input',
         message: 'What is the title of your repository?',
@@ -26,14 +23,46 @@ inquirer
         type: 'input',
         message: 'What is the description of your application?',
         name: 'description',
+        validate: your_description => {
+            if (your_description) {
+                return true;
+            } else {
+                console.log('Enter A Description!');
+                return false;
+            }
+        }
     },
     {
         type: 'checkbox',
         message: 'What license will you be using for you repository?',
         name: 'license',
-        choices: ['MIT', ''],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
-    }, 
-  ]);
+        choices: ['MIT', 'GNU', 'Apache', 'none of the above'],
+        validate: your_license => {
+            if (your_license) {
+                return true;
+            } else {
+                console.log("");
+                return false;
+            }
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+    },
+    {
+        type: 'input',
+        message: 'How do you install the application?',
+        name: 'install',
+        validate: your_install => {
+            if (your_install) {
+                return true;
+            } else {
+                console.log('explain how to install the application please');
+                return false;
+            }
+        }
+    },
+    {
+
+    },
+  ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,7 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
- // ![badge](https:img.shields.io/badge/license-${answers.license}-neongreen)
+// [![badge](https:img.shields.io/badge/license-${answers.license}-blue.svg)]
 }
 
 // TODO: Create a function that returns the license link
@@ -15,7 +15,35 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-##
+  <h1 align="center">${answers.title} 👋</h1>
+
+  ## Licenses: 
+![badge](https:img.shields.io/badge/license-${answers.license}-blue.svg)
+🔍 ${answers.description}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#use)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+## Description:
+${answers.description}
+## Installation
+${answers.installation}
+## Usage
+${answers.use}
+## License
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+<br />
+This application uses the ${answers.license} license. 
+## Contributing
+${answers.contribution}
+## Tests
+${answers.tests}
+## Questions
+${answers.questions}
 `;
 }
 
