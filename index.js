02ea68277a1b38e35f48c520cf0520f66b7ff09f
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-08-24 00:18:24 -0500

    added more questions and choices

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs'); 
+const fse = require('fs-extra');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -12,8 +13,21 @@ inquirer
         message: 'What is your name?',
     },
     {
-        type: 'input'
-    }
+        type: 'input',
+        message: 'What is the title of your repository?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your application?',
+        name: 'description',
+    },
+    {
+        type: 'checkbox',
+        message: 'What license will you be using for you repository?',
+        name: 'license',
+        choices: ['']
+    },
   ])
 
 // TODO: Create a function to write README file
