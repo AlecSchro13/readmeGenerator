2c549284e569809840f21bb73b2945f88d48ccb2
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-08-23 23:09:52 -0500

    added questions to the index.js:

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs'); 
 
 // TODO: Create an array of questions for user input
 const questions = [];
+inquirer
+  .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input'
+    }
+  ])
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
