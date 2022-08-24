7acf86ec45614fdcb47b11d1e7a5d21966a98b89
Alec Schroetlin <alec.schroetlin1@gmail.com>
2022-08-24 12:37:11 -0500

    added const for generate Markdown

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const inquirer = require('inquirer');
 const fse = require('fs-extra');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
